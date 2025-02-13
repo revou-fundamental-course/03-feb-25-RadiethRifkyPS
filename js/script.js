400268305239881bb4f95fcc67a4857100987393
Radieth Rifky Pramadya S <goldenam255@gmail.com>
2025-02-13 21:23:49 +0700

    Merapikan Javascript

--- js/script.js
+++ js/script.js
@@ -2,24 +2,18 @@ function konversi() {
     let input = document.getElementById('main-input');
 
     if (input.value == '') {
-        alert('tolong isi inputan') ;
+        alert('Isi input angka terlebih dahulu!') ;
     } else {
-        // console.log('Berhsil')
         const celsius = document.getElementById('main-input').value;
         const fahrenheit = (celsius * 9/5) + 32;
         document.getElementById('main-result').value = fahrenheit;
-        // document.getElementById('cara-konversi').value = document.write("S<sub>(&deg;F) </sub> = (S<sub>(&deg;C) </sub> &times; 9/5) + 32");
     }
 }
 
-function reset1() {
+//Mereset nilai input dan result
+function reset1() { 
     let input1 = document.getElementById('main-input');
     input1.value = '';
     let input = document.getElementById('main-result');
     input.value = '';
-
-
-
-    // document.getElementById('main-input').reset1();
-    // document.getElementById('main-result').reset1();
 }
